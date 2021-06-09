19fba82b4191b638cc08a31c3a0addf3b77c261e
NicolasSompairac <nsompairac@gmail.com>
2021-06-09 14:29:06 +0200

    Update version to 0.0.2
    Former-commit-id: 9b17bd105a2948d96580359339cec88baa2c9a73

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hica",
-    version="0.0.1",
+    version="0.0.2",
     author="Nicolas Sompairac",
     author_email="nicolas.sompairac@curie.fr",
     description="Hierarchical ICA algorithm and applications to genomic data",
