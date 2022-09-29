1c1ce2ccae41f628ac711118a7f66611b434b34b
Nicolas Sompairac <nsompairac@gmail.com>
2022-09-29 21:28:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,17 +14,17 @@ setuptools.setup(
     url="https://github.com/NicolasSompairac/HACK",
     packages=setuptools.find_packages(),
     install_requires = [
-        "scipy == 1.6.3",
+        "scipy >= 1.6.3",
         "scanpy >= 1.4.5.post2",
-        "pandas == 1.2.4",
-        "numpy == 1.20.3",
-        "networkx == 2.4",
-        "matplotlib == 3.4.1",
-        "jupyterthemes == 0.20.0",
-        "ipywidgets == 7.4.2",
-        "requests == 2.22.0",
-        "json2html == 1.3.0",
-        "mygene==3.2.2"
+        "pandas >= 1.2.4",
+        "numpy >= 1.20.3",
+        "networkx >= 2.4",
+        "matplotlib >= 3.4.1",
+        "jupyterthemes >= 0.20.0",
+        "ipywidgets >= 7.4.2",
+        "requests >= 2.22.0",
+        "json2html >= 1.3.0",
+        "mygene >= 3.2.2"
         ],
     classifiers=[
         "Programming Language :: Python :: 3",
