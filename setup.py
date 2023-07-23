03903b5ed99c682aa915664c4d43771cb21d72b4
Nicolas Sompairac <nsompairac@gmail.com>
2023-07-23 09:17:14 +0100

    Update setup.py
    Update liste of requirements to higher version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hack",
-    version="0.0.4",
+    version="0.0.5",
     author="Nicolas Sompairac",
     author_email="nicolas.sompairac@curie.fr",
     description="Hierachical Analysis of Component linKs (HACK) algorithm for applications to genomic data",
@@ -14,15 +14,16 @@ setuptools.setup(
     url="https://github.com/NicolasSompairac/HACK",
     packages=setuptools.find_packages(),
     install_requires = [
-        "scipy == 1.6.3",
-        "scanpy >= 1.4.5.post2",
-        "pandas == 1.2.4",
-        "numpy == 1.20.3",
-        "networkx == 2.4",
-        "matplotlib == 3.4.1",
+        "scipy == 1.9.1",
+        "scanpy >= 1.9.1",
+        "stabilized-ica == 2.0.0",
+        "pandas == 1.5.0",
+        "numpy == 1.23.3",
+        "networkx == 2.8.6",
+        "matplotlib == 3.6.0",
         "jupyterthemes == 0.20.0",
         "ipywidgets == 7.4.2",
-        "requests == 2.22.0",
+        "requests == 2.28.1",
         "json2html == 1.3.0",
         "mygene==3.2.2"
         ],
@@ -31,5 +32,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU GPL License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8.13',
 )
